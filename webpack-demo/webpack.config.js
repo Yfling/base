7e704c1633cdca835262ffc3a297872e799738ad
Yfling <598184085@qq.com>
2017-09-11 17:20:36 +0800

    生成Source Maps（使调试更容易）

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  devtool: 'eval-source-map',  // source maps需要配置devtool
   entry: __dirname + "/app/main.js",  // 唯一入口文件
   output: {
     path: __dirname + "/public", // 打包后的文件存放的地方
