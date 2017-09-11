5efad1e084f7a77a6acc46ba05cd94838e1deaa9
Yfling <598184085@qq.com>
2017-09-11 18:51:58 +0800

    缓存

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
         path: __dirname + "/build",
-        filename: "bundle.js"
+        filename: "bundle-[hash].js"
     },
     devtool: 'eval-source-map',
     devServer: {
