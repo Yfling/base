c151044914221a45c5677c83a4a5978ab7b019eb
Yfling <598184085@qq.com>
2017-09-11 18:10:34 +0800

    CSS

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -22,6 +22,17 @@ module.exports = {
           loader: "babel-loader",
         },
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
       }
     ]
   }
