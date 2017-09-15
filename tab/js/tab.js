665c62315916be3662862ea1b56961b8cf930b12
Yfling <598184085@qq.com>
2017-09-15 15:17:47 +0800

    切换功能实现

--- tab/js/tab.js
+++ tab/js/tab.js
@@ -1,6 +1,6 @@
 ;(function ($) {
   var Tab = function (tab) {
-    var _this = this;
+    var that = this;
     // 保存单个tab组件
     this.tab = tab;
     this.config = {
@@ -28,18 +28,41 @@
 
     if (config.triggerType === "click") {
       this.tabItems.bind(config.triggerType, function () {
-        alert(1);
+        that.invoke($(this));
       });
     }
     else if (config.triggerType === "mouseover" || config.triggerType !== "click") {
       this.tabItems.mouseover(function () {
-        alert(2);
+        that.invoke($(this))
       })
     }
 
   };
 
   Tab.prototype = {
+    // 事件驱动函数
+    invoke: function (currentTab) {
+       var that = this;
+       /**
+        *要执行Tab的选中状态，当前选中的加上actived（标记为白底）
+        *切换对应的tab内容，要根据配置参数的effect是default还是false
+        **/
+        var index = currentTab.index();
+        // tab选中状态
+        currentTab.addClass("actived").siblings().removeClass("actived");
+        // 切换对应的内容区域
+        var effect = this.config.effect;
+        var conItems = this.contentItems;
+
+        if (effect === "default" || effect !== "fade") {
+          console.log('haha')
+          conItems.eq(index).addClass("current").siblings().removeClass("current");
+        }
+        else if (effect === "fade") {
+          console.log('fade')
+          conItems.eq(index).fadeIn().siblings().fadeOut();
+        }
+    },
     // 获取配置参数
     getConfig: function () {
       // 拿一下tab elem节点上的data-config
