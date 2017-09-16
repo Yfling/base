42f333cc71dd76f75d4fed48c32c2ab387a6f217
Yfling <598184085@qq.com>
2017-09-16 23:16:00 +0800

    数据渲染

--- datepicker/date.js
+++ datepicker/date.js
@@ -12,16 +12,19 @@
       month = today.getMonth() + 1;
     }
     var firstDay = new Date(year, month - 1, 1);
-    var firstDayWeekDay = firstDay.getDate();
+    var firstDayWeekDay = firstDay.getDay();
     if (firstDayWeekDay === 0) firstDayWeekDay = 7;
 
     var lastDayOfLastMonth = new Date(year, month - 1, 0);
-    var lastDateOfLastMonth = lastDayOfLastMonth.getDate();
+    var lastDateOfLastMonth = lastDayOfLastMonth.getDay();
+
+    year = firstDay.getFullYear();
+    month = firstDay.getMonth() + 1;
 
     var preMonthDayCount = firstDayWeekDay - 1;
 
     var lastDay = new Date(year, month, 0);
-    var lastDate = lastDay.getDate();
+    var lastDate = lastDay.getDay();
 
     for (var i = 0; i < 7 * 6; i++) {
       var date = i + 1 - preMonthDayCount;
@@ -46,8 +49,12 @@
         showDate: showDate
       });
     }
-    return ret;
-  }
+    return {
+      year: year,
+      month: month,
+      days: ret
+    };
+  };
 
   // 唯一暴露的对象
   window.datepicker = datepicker;
